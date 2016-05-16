9249e5b17d4d5c89a414cb54c243d769902d5b42
Alexqq11 <alt.alexqq11@gmail.com>
2016-05-17 00:18:25 +0500

    refactor: decomposition

---
+++ potsGames/potsGames/Obstracles.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Media;
+
+namespace potsGames
+{
+	public static class Rnd
+	{
+		static Random rnd = new Random();
+		static public int GetRandomNumber(int start, int end)
+		{
+
+			return rnd.Next(start, end);
+		}
+	}
+
+	public class Obstacle
+	{
+		public int ObstacleWidth { get; set; }
+		public int ObstacleVerticalInterval { get; set; }
+		public int WindowHeight { get; set; }
+
+		public int TopX { get; set; }
+		public int TopY { get; set; }
+		public int BottomX { get; set; }
+		public int BottomLength { get; set; }
+		// сильно зависим от параметров окна //
+
+		public Obstacle(int windowHeight, int windowWidth, int pipeWidth, int distanceBetweenPipes) // To do make normal params to generate pipe 
+		{
+			TopX = windowWidth;//sreen possition
+
+
+
+			TopY = Rnd.GetRandomNumber(40, (windowHeight - distanceBetweenPipes)); // refact 40 to pipe length min
+			Console.WriteLine(TopY);
+			BottomX = windowWidth;//screen posirion
+			BottomLength = TopY + distanceBetweenPipes;
+			ObstacleWidth = pipeWidth;
+			ObstacleVerticalInterval = distanceBetweenPipes;
+			WindowHeight = windowHeight;
+		}
+
+		public void DrawPipe(PaintEventArgs e)
+		{
+			var bottomPipe = Properties.Resources.GetImage("yellow_pipe");
+			var topPipe = Properties.Resources.GetImage("yellow_pipe");
+			topPipe.RotateFlip(RotateFlipType.RotateNoneFlipXY);
+			e.Graphics.DrawImage(topPipe, TopX, 0, ObstacleWidth, TopY + 15);
+			e.Graphics.DrawImage(bottomPipe, BottomX, BottomLength, ObstacleWidth, WindowHeight - BottomLength);
+
+		}
+		public void DrawTest(PaintEventArgs e)
+		{
+			//e.Graphics.DrawImage(Image i);
+		}
+		//public void DrawBox(PaintEventArgs)
+
+	}
+
+	public class ObstaclesMap
+	{
+
+		int ObstacleWidth { get; set; }         //= 55;
+		int ObstacleVerticalInterval { get; set; }    //= 140;
+		int ObstaclesHorizontInterval { get; set; }
+		public int Count { get; set; } //= 180;
+		int WindowWidth { get; set; }
+		int WindowHeight { get; set; }
+		int Speed { get; set; }
+
+		public List<Obstacle> LevelObstacles { get; set; }
+		public ObstaclesMap(int windowHeight, int windowWidth, int obstracleWidth, int horizontInterval, int verticalInterval)
+		{
+			WindowHeight = windowHeight;
+			WindowWidth = windowWidth;
+			Count = (windowWidth - obstracleWidth * (windowWidth / horizontInterval)) / horizontInterval + 1; // in the future write normal stabilization
+			ObstacleWidth = obstracleWidth;
+			ObstaclesHorizontInterval = horizontInterval;
+			ObstacleVerticalInterval = verticalInterval;
+			MakeObstacles();
+		}
+		private void MakeObstacles()
+		{
+
+			LevelObstacles = new List<Obstacle>();
+
+			for (var i = 0; i < Count; i++)
+			{
+				var pipe = new Obstacle(WindowHeight, WindowWidth + i * ObstaclesHorizontInterval, ObstacleWidth, ObstacleVerticalInterval);
+				LevelObstacles.Add(pipe);
+			}
+		}
+		public void UpdateObstracles()
+		{
+			for (var i = 0; i < Count; i++)
+			{
+				if (LevelObstacles.Any())
+					LevelObstacles[i] = new Obstacle(WindowHeight, WindowWidth + i * ObstaclesHorizontInterval, ObstacleWidth, ObstacleVerticalInterval);
+			}
+		}
+
+		public Obstacle this[int i] // write exception
+		{
+			get { return LevelObstacles[i]; }
+			set { LevelObstacles[i] = value; }
+		}
+	}
+
+}
---
+++ potsGames/potsGames/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Media;
+
+namespace potsGames
+{
+
+	public class Player
+	{
+		public int lives { get; set; }
+		public int coins { get; set; }
+		public bool Alive { get; set; }
+		public Point StartLocation { get; set; }
+		public int SpeedY { get; set; }
+		public int SpeedX { get; set; }
+		public bool inPointZone { get; set; }
+		public Player(int speedX, int speedY, Point startLocation)
+		{
+			Alive = true;
+			SpeedX = speedX;
+			SpeedY = speedY;
+			inPointZone = false;
+			StartLocation = startLocation;
+
+		}
+
+	}
+}
